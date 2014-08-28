c1e4062c81b7c5cf761120baac61ce6b61e0e3e3
Lawrence Jones <lawrjone@amazon.com>
2014-08-28 02:21:07 -0700

    Adds post-merge hook

--- symlink.sh
+++ symlink.sh
@@ -3,19 +3,30 @@
 dir=~/dotfiles
 olddir=~/dotfiles_old
 
+# Symlink hooks to .git/hooks
+if [ ! -L .git/hooks ];
+then
+  echo Creating hooks symlink...
+  rm -rf .git/hooks && ln -s ../hooks ./.git/hooks && echo -e "...done\n"
+fi
+
+# If the old backup exists, chuck it out!
 if [ -d $olddir ];
 then
-  echo Cleaning old backup
+  echo Cleaning old backup...
   rm -rf $olddir
+  echo -e "...done\n"
 fi
+
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
+echo -e "...done\n"
 
-echo "Changing to the $dir directory"
+echo -e "Changing to the $dir directory...\n"
 cd $dir
-echo "...done"
 
-for f in `ls | grep -iv "symlink"`; do
+# For every file that is not symlink or hooks
+for f in `ls | grep -iv -E "hooks|symlink.sh"`; do
     if [ -a ~/.$f ];
     then
       mv ~/.$f ~/dotfiles_old/
@@ -25,10 +36,12 @@ for f in `ls | grep -iv "symlink"`; do
     ln -s $dir/$f ~/.$f
 done
 
+echo -e "\n...done\n"
+
 # If vundle if not already present, clone
 [[ -d $dir/vim/bundle/Vundle.vim ]] || (
   echo Installing vundle...
   git clone https://github.com/gmarik/Vundle.vim $dir/vim/bundle/Vundle.vim
 )
 
-echo Done!
+echo Complete!
