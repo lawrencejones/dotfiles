f9ec8b5d37ccd2c0e5e2ca9680cce2e4ab38a524
Lawrence Jones <lawrjone@gmail.com>
2015-07-16 14:18:44 +0100

    Fixes pp.js, adds zmv, vim plugins

--- bin/pp.js
+++ bin/pp.js
@@ -4,7 +4,8 @@ var input = "";
 
 function lexisort(obj) {
   if (obj instanceof Array) return obj.map(lexisort);
-  if ('object' != typeof obj) return obj;
+  if (obj === null) return null;
+  if ('object' !== typeof obj) return obj;
 
   var sortedObj = {};
   Object.keys(obj).sort().map(function(key) {
