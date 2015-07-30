c9be3d29ddf3d30e8f3e5abb9a0779deb225c06c
Lawrence Jones <lawrjone@gmail.com>
2015-07-30 10:45:13 +0100

    Clean up pane management, fix edge case

--- bin/vim-tmux-move.bash
+++ bin/vim-tmux-move.bash
@@ -3,14 +3,14 @@
 program="`tmux display -p '#{pane_current_command}'`"
 
 if [[ $program == "vim" ]]; then
-	# let vim handle it
-	tmux send-keys 'Escape' 'C-w' $1
+  # let vim handle it
+  tmux send-keys 'Escape' ":call TmuxMove('" $1 "')" Enter
 else
-	# do the normal tmux thing
-	case $1 in
-		j) tmux select-pane -D ;;
-		k) tmux select-pane -U ;;
-		h) tmux select-pane -L ;;
-		l) tmux select-pane -R ;;
-	esac
+  # do the normal tmux thing
+  case $1 in
+    j) tmux select-pane -D ;;
+    k) tmux select-pane -U ;;
+    h) tmux select-pane -L ;;
+    l) tmux select-pane -R ;;
+  esac
 fi
